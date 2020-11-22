0c417f2df4ff043d8d37ed62f9b2debe5d4d32ca
Eduardo Luz <eduluz1976@gmail.com>
2020-11-22 00:18:48 -0800

    Adding more complex structures

--- src/Auditor.php
+++ src/Auditor.php
@@ -24,21 +24,52 @@ class Auditor
 
     protected function handleGetVariables()
     {
-        if (isset($_GET)) {
-            foreach ($_GET as $k => $v) {
-                $item = $this->evaluate($k, $v);
-                $this->getDriver()->addItem($item);
+        $listSuperVars = ['_GET', '_POST', '_COOKIE'];
+
+        foreach ($listSuperVars as $varName) {
+            if (isset($GLOBALS[$varName])) {
+                foreach ($GLOBALS[$varName] as $k => $v) {
+                    if (\is_scalar($v)) {
+                        $item = $this->evaluate($k, $v);
+                        $this->getDriver()->addItem($item);
+                    } elseif (is_array($v)) {
+                        $item = $this->evaluateArray($k, $v);
+                        $this->getDriver()->addItem($item);
+                    }
+                }
             }
         }
     }
 
-    protected function evaluate($k, $v)
+    protected function evaluateArray($k, $v)
+    {
+        $evalResult = [
+            'name' => $k,
+            'type' => 'array',
+        ];
+        foreach ($v as $k2 => $v2) {
+            if (\is_scalar($v2)) {
+                $item = $this->evaluate($k2, $v2, $k);
+                $this->getDriver()->addItem($item);
+            } elseif (is_array($v2)) {
+                $item = $this->evaluateArray($k . '/' . $k2, $v2);
+                $this->getDriver()->addItem($item);
+            }
+        }
+        return $evalResult;
+    }
+
+    protected function evaluate($k, $v, $scope = null)
     {
         $evalResult = [
             'name' => $k,
             'value' => $this->encode('' . $v),
             'inferred_types' => [],
         ];
+
+        if ($scope) {
+            $evalResult['scope'] = $scope;
+        }
 
         if (\is_scalar($v)) {
             $evalResult['length'] = strlen('' . $v);
--- tests/functional/AuditTest.php
+++ tests/functional/AuditTest.php
@@ -9,6 +9,14 @@ class AuditTest extends TestCase
     protected $driver;
     protected $auditor;
 
+    protected function tearDown()
+    {
+        $_GET = [];
+        $_POST = [];
+        $_JSON = [];
+        $_COOKIE = [];
+    }
+
     public function getDriver($clear = false)
     {
         if (!$this->driver || $clear) {
@@ -49,5 +57,63 @@ class AuditTest extends TestCase
             $this->assertEquals(strlen('' . $_GET[$name]), $entry['length']);
             $this->assertEquals($_GET[$name], $this->getAuditor()->decode($entry['value']));
         }
+    }
+
+    public function testPostVariable()
+    {
+        $_POST = [
+            'email' => 'john.doe@company.com',
+            'user_id' => 12345
+        ];
+
+        $this->getAuditor(true)->handle();
+        $lines = $this->getDriver()->getData();
+        $this->assertTrue(is_array($lines));
+        $this->assertCount(count($_POST), $lines);
+
+        foreach ($lines as $k => $entry) {
+            $name = $entry['name'];
+            $this->assertEquals(strlen('' . $_POST[$name]), $entry['length']);
+            $this->assertEquals($_POST[$name], $this->getAuditor()->decode($entry['value']));
+        }
+    }
+
+    public function testGetPostVariables()
+    {
+        $_GET = [
+            'user_id' => 123
+        ];
+
+        $_POST = [
+            'email' => 'john.doe@company.com',
+            'user_id' => 12345
+        ];
+
+        $this->getAuditor(true)->handle();
+        $lines = $this->getDriver()->getData();
+        $this->assertTrue(is_array($lines));
+        $this->assertCount(count($_POST) + count($_GET), $lines);
+    }
+
+    public function testComplexPostVariables()
+    {
+        $_POST = [
+            'email' => 'john.doe@company.com',
+            'user_id' => 12345,
+            'data' => [
+                'auth' => ['username' => 'abc', 'password' => 'xyz'],
+                'metadata' => [
+                    'nested' => [
+                        'email' => 'abc@x.com'
+                    ]
+                ]
+            ]
+        ];
+
+        $this->getAuditor(true)->handle();
+        $lines = $this->getDriver()->getData();
+
+        $this->assertTrue(is_array($lines));
+        $this->assertCount(9, $lines);
     }
 }
